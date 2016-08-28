8450a0391558b672f14c7aac9a364251ceaf092a
chaossssss <1033729276@qq.com>
2016-08-28 18:39:03 +0800

    服务地址

--- src/js/main.js
+++ src/js/main.js
@@ -6,35 +6,49 @@ $(function(){
   var $pwdNum = $(".login-pwdnum");
   var $loginPwd = $("#loginPwd");
   var $toggleBtn = $("#toggle-btn");
+  var $btnlog = $("#btnlog");
+  var $tip = $("#loginTip");
+
   $phoneNum.blur(function(){
-    if($phoneNum.val() === ''){
-      console.log('手机号为空');
-    }
+    // var phone = $phoneNum.val();
+    // if(phone === ''){
+    //   console.log('手机号为空');
+    // }
+    // if(!(/^1[3|4|5|7|8]\d{9}$/.test(phone))){
+    //   console.log('错误的手机号');
+    //   $tip.text('账号错误');
+    //   $tip.removeClass("btnhidden");
+    //   $tip.addClass("login-tip");
+    // }
     $loginPhone.removeClass("login-blur");
     $loginPhone.addClass("login-phone");
-    // $cleanBtn.attr("class","btnhidden");输入手机号图标消失有问题
-  })
+    // $cleanBtn.removeClass();
+    // $cleanBtn.attr("class","btnhidden");//输入手机号图标消失有问题
+  });
   $phoneNum.click(function(){
     $loginPhone.addClass("login-blur");
     $cleanBtn.attr("class","cleanbtn");
     $cleanBtn.click(function(){
        $phoneNum.val('');
        $(this).attr("class","btnhidden");
-    })
+    });
   });
 
   $pwdNum.blur(function(){
-    if($pwdNum.val() === ''){
-      console.log('密码为空');
-    }
-    $cleanBtn.attr("class","btnhidden");
+    // if($pwdNum.val() === ''){
+    //   console.log('密码为空');
+    // }
+    // $cleanBtn.attr("class","btnhidden");
     $loginPwd.removeClass("login-blur");
     $loginPwd.addClass("login-pwd");
     // $toggleBtn.attr("class","btnhidden");
+    // $toggleBtn.removeClass();
   });
   $pwdNum.click(function(){
+    $cleanBtn.attr("class","btnhidden");
     $loginPwd.addClass("login-blur");
     $toggleBtn.attr("class","pwdshow");
+  });
     $toggleBtn.click(function(){
       if($toggleBtn.attr("class") == "pwdshow"){
         $toggleBtn.attr("class","pwdhidden");
@@ -44,5 +58,29 @@ $(function(){
         $pwdNum.attr("type","text");
       }
     })
-  });
+
+  $btnlog.click(function(){
+    console.log($phoneNum.val());
+    console.log($pwdNum.val());
+    $tip.text('');
+    $tip.removeClass();
+
+    $.ajax({
+      method:'post',
+      url:'http://wapapi.zhujiash.com/api/v1/clientinfo/Login',
+      data:{
+        LoginName:$phoneNum.val(),
+        Password:$pwdNum.val()
+      },
+      success:function(data){
+        if(data.Meta.ErrorCode == '0'){
+          console.log('成功');
+        }else{
+          console.log(data.Meta);
+          $tip.attr("class","login-tip");
+          $tip.text(data.Meta.ErrorMsg);
+        }
+      }
+    });
+  })
 })
