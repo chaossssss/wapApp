09d2aeea45b3787964fa42f967ffca483824d53a
zhouhao <2153229687@qq.com>
2016-09-09 15:06:45 +0800

    1.订单支付 2.确认订单

---
+++ src/js/pay/pay.js
@@ -0,0 +1,29 @@
+"use strict"
+angular.module('com.wapapp.app',[])
+.controller('payCtrl',['$scope',function($scope){
+	var vm = $scope.vm = {};
+
+	
+
+
+
+
+}])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
