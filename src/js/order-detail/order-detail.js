b0597416b8d9673a8a0e2c5b92c86264099d79de
chaossssss <1033729276@qq.com>
2016-09-13 19:49:00 +0800

    我的订单和订单详情。。。。。。爆炸

--- src/js/order-detail/order-detail.js
+++ src/js/order-detail/order-detail.js
@@ -21,23 +21,33 @@ $(function(){
       orderStatus = data.body[0].OrderStatus;
       picData = data.body[0].OrderService[0].Pictures;
       picLength = data.body[0].OrderService[0].Pictures.length;
-      var gender = data.body[0].ServiceProviderGender;
+      // var gender = data.body[0].ServiceProviderGender;
       orderIsGeted = data.body[0].ServiceProviderId;
 
+      // data.body[0].OrderId;
       $("#orderCode").text(data.body[0].OrderCode);
       $("#createTime").text(data.body[0].CreateTime);
+      // data.body[0].OrderStatus;
+      $("#acceptAt").text(data.body[0].AcceptAt);
+      $("#finishAt").text(data.body[0].FinishAt);
+      // data.body[0].ConfirmAt;
+      // data.body[0].CancelAt;
+      // data.body[0].IsPauOff;
+      $("#price").text(data.body[0].price);
+      $("#discountInfo").text(data.body[0].DiscountInfo);
+      // data.body[0].PayLock;
+      // data.body[0].ServiceProviderId;
+      $("#serviceProviderName").text(data.body[0].ServiceProviderName);
+      // data.body[0].ServiceProviderGender;
+      // data.body[0].ServiceProviderPhone;
       $("#clientName").text(data.body[0].ClientName);
       $("#clinetPhone").text(data.body[0].ClinetPhone);
       $("#serviceAddress").text(data.body[0].ServiceAddress);
       $("#serviceTime").text(data.body[0].ServiceTime);
-      $("#serviceProviderName").text(data.body[0].ServiceProviderName);
-      $("#acceptAt").text(data.body[0].AcceptAt);
-      $("#price").text(data.body[0].price);
-      $("#discountInfo").text(data.body[0].DiscountInfo);
-      $("#finishAt").text(data.body[0].FinishAt);
+      // data.body[0].CanCancel;
 
-      var workHeadPic = data.body[0].WorkerHead;
-      $("#workerHead").attr("src",workHeadPic);
+      // var workHeadPic = data.body[0].WorkerHead;
+      // $("#workerHead").attr("src",workHeadPic);
 
       for(i = 0; i < picLength; i++ ){
         console.log(picData[i].SmallPic);
@@ -46,23 +56,14 @@ $(function(){
         $("#showPics").append(html);
       }
 
-      if(gender == "1"){
-        $("#workerGender").text('阿姨');
-      }else if(gender == "0"){
-        $("#workerGender").text("师傅");
-      }
+      // if(gender == "1"){
+      //   $("#workerGender").text('阿姨');
+      // }else if(gender == "0"){
+      //   $("#workerGender").text("师傅");
+      // }
       // function getOrderStatus(orderStatus){
       //   return orderStatus;
       // }
-
-    // switch(orderStatus){
-    // case "10":
-    // console.log("待接单");
-    // break;
-    // case "11":
-    // console.log("代付款");
-    // break;
-  //}
 
     }
   })
@@ -101,6 +102,8 @@ console.log(orderStatus);
     $("#payTime").hide();
     $("#btnLeft").hide();
     $("#cancelTime").hide();
+    $("#specialPrice").hide();
+    $("#waitOrder").hide();
 
     $("#orderTime").css("marginBottom","4px");
     $("#servicePrice").css("marginBottom","4px");
@@ -256,6 +259,14 @@ console.log(orderStatus);
 
     case "20":
     console.log("已接单");
+
+    // if(hasPaied == "0"){
+    //   console.log("订单状态：未付清");
+    // }
+    // if(hasPaied == "1"){
+    //   console.log("订单状态：付清");
+    // }
+
     $("#orderStatus").css("background-image","url(../../images/order-detail/get-order.png)");
 
     $("#status").text('工人已接单');
@@ -272,7 +283,8 @@ console.log(orderStatus);
     $("#roundSecond").addClass("round-complete");
     $("#roundThird").addClass("round-processing");
     $("#roundFourth").addClass("round-undone");
-    $("#total").addClass("actual");
+    // $("#total").addClass("actual");
+    $("#wait").addClass("actual");
     $("#btnLeft").addClass("delete-btn")
     $("#btnRight").addClass("pay-btn");
 
@@ -283,7 +295,7 @@ console.log(orderStatus);
     $("#negotiable").hide();
     $("#cancelTime").hide();
 
-    $("#orderPrice").css("marginBottom","0px");
+    $("#waitOrder").css("marginBottom","0px");
     $("#btnRight").css("left","181px");
 
     $("#btnLeft").click(function(){
@@ -347,7 +359,7 @@ console.log(orderStatus);
     // var time = "2016/09/12 11:16:30";
     // var ftime = new Date(time).Format("yyyy-MM-dd hh:mm:ss");
 
-    function DateDiff(sDate1, sDate2){//sDate1和sDate2是2006-12-18格式  
+    function DateDiff(sDate1, sDate2){//sDate1和sDate2是yyyy-MM-dd格式  
         var aDate, oDate1, oDate2, iDays, iHours, diffText;
         aDate = sDate1.split("-");
         oDate1 = new  Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]); 
@@ -358,7 +370,7 @@ console.log(orderStatus);
         diffText = iDays + "天" + iHours + "时";
         return  diffText; 
     }
-    var dateDiff = DateDiff(now,confirmTime);
+    var dateDiff = DateDiff(confirmTime,now);
     var dataDiffText = "请确认服务完成，还剩" + dateDiff + "自动确认";
     console.log(dataDiffText);
     console.log(now);
@@ -389,6 +401,39 @@ console.log(orderStatus);
     $("#negotiable").hide();
     $("#btnLeft").hide();
     $("#cancelTime").hide();
+    $("#specialPrice").hide();
+    $("#waitOrder").hide();
+
+    break;
+
+    case "40":
+    console.log("客户确认完成，待评价");
+
+    $("#orderStatus").css("background-image","url(../../images/order-detail/awaiting-assessment.png)");
+
+    $("#status").text('服务已完成');
+    $("#explanation").text('评价一下这次服务吧~');
+    $("#tabFirst").text('订单已提交');
+    $("#tabSecond").text('工人已接单');
+    $("#tabThird").text('服务完成');
+    $("#btnRight").text('评价');
+
+    $("#tabThird").addClass("processing");
+    $("#roundFirst").addClass("round-complete");
+    $("#roundSecond").addClass("round-complete");
+    $("#roundThird").addClass("round-processing");
+    $("#btnRight").addClass("confirm-btn");
+
+    $(".round").css("left","58px");  
+
+    $("#proFourth").hide();
+    $("#refundRecord").hide();
+    $("#filling2").hide();
+    $("#negotiable").hide();
+    $("#btnLeft").hide();
+    $("#cancelTime").hide();
+    $("#specialPrice").hide();
+    $("#waitOrder").hide();
 
     break;
 
@@ -472,19 +517,166 @@ console.log(orderStatus);
     
     break;
 
+    /*--退款状态--*/
+    if(refundIsFinshed == "1"){
+      console.log("退款中");
+      $("#orderStatus").css("background-image","url(../../images/order-detail/refund.png)");
+
+      $("#status").text('退款中');
+      $("#explanation").text('系统将于72小时内退款');
+      $("#tabFirst").text('订单已提交');
+      $("#tabSecond").text('工人已接单');
+      $("#tabThird").text('退款中');
+      $("#tabFourth").text('退款成功');
+      $("#refundStatus").text('退款中');
+
+      $("#tabThird").addClass("processing");
+      $("#roundFirst").addClass("round-complete");
+      $("#roundSecond").addClass("round-complete");
+      $("#roundThird").addClass("round-processing");
+      $("#roundFourth").addClass("round-undone");
+
+      $("#optionFooter").hide();
+      $("#negotiable").hide();
+      $("#cancelTime").hide();
+
+
+    }
+    if(refundIsFinshed == "2"){
+      console.log("退款完成");
+
+      $("#status").text('订单已取消');
+      $("#explanation").text('');
+      $("#tabFirst").text('订单已提交');
+      $("#tabSecond").text('工人已接单');
+      $("#tabThird").text('退款成功');
+      $("#tabFourth").text('订单已取消');
+      $("#refundStatus").text('退款完成');
+
+      // $("#tabThird").addClass("processing");
+      $("#roundFirst").addClass("round-undone");
+      $("#roundSecond").addClass("round-undone");
+      $("#roundThird").addClass("round-undone");
+      $("#roundFourth").addClass("round-undone");
+    }
   }
 
-  // $.ajax({
-  //   method:"POST",
+  $.ajax({
+    method:"POST",
+    url:"http://192.168.1.191:3001/api/v2/Provider/Detail",
+    data:{
+      Type:"1",
+      Id:"59"
+    },
+    success:function(data){
+      console.log(data.Body.Worker);
+      console.log(data.Body.Business);
+      if(data.Body.Worker != null){
+        console.log("工人");
+        var workHeadPic = data.Body.Worker.Photo;
+        var workName = data.Body.Worker.Name;
+        // var gender = data.Body.ServiceProviderGender;
+        var gender = data.Body.Worker.Gender;
+        console.log(gender);
+        $("#providerHead").attr("src",workHeadPic);
+        $("#serviceProviderName").text(workName);
+        if(gender == "1"){
+          $("#workerGender").text('阿姨');
+        }else if(gender == "0"){
+          $("#workerGender").text("师傅");
+        }
+      }
+      if(data.Body.Business != null){
+        console.log("商户");
+        var busHeadPic = data.Body.Business.Photo;
+        $("#providerHead").attr("src",busHeadPic);
+      }
+    }
+  });
+/*
+  $.ajax({
+     method:"POST",
+     url:"http://192.168.1.191:3003/api/v2/OrderInfo/GetOrderInfoEx",
+     data:{
+      Token:"",
+      OrderId:""
+     },
+     success:function(data){
+       console.log('2222');
+     }
+   });*/
 
-  // });
+/*
+  $.ajax({
+    method:"POST",
+    url:"http://192.168.1.191:3003/api/v2/SystemServiceController/GetServicePriceEx",
+    data:{
+      Token:"",
+      ServiceTypeId:"",
+      ServiceProviderType:""
+    },
+    success:function(data){
+      data.body[0].Min;
+      data.body[0].Max;
+      data.body[0].unit;
+    }
+  });*/
 
-  // $.ajax({
-  //    method:"POST",
-  //    url:"http://192.168.1.191:7002/api/v2/OrderInfoController/GetOrderInfoEx",
-  //    success:function(data){
-  //      console.log('2222');
-  //    }
-  //  })
+  function removeOrder(){
+    console.log("删除订单");
+    $.ajax({
+
+    })
+  }
+
+  /*--添加备注--*/
+  $("#addRemark").click(function(){
+    console.log("显示弹窗");
+    $("#userMemo").show();
+  });
+  $("#confirmBtn").click(function(){
+    var memoText = $("#memoText").val();
+    console.log(memoText);
+    if(memoText != ""){
+      memo();
+    }else{
+      console.log("不提交");
+    }
+  })
+  $("#cancelBtn,#confirmBtn").click(function(){
+    $("#userMemo").hide();
+  });
+  // $("#addRemark").on("click",function(){
+  //   console.log("显示弹窗");
+  //   $("#userMemo").show();
+  // })
+  function memo(){
+    console.log("补充备注");
+    $.ajax({
+
+    })
+  }
+  function cancelOrder(){
+    console.log("取消订单");
+    $.ajax({
+
+    })
+  }
+  function confirmOrder(){
+    console.log("确认订单");
+    $.ajax({
+
+    })
+  } 
+  function completeOrder(){
+    console.log("完成订单");
+    $.ajax({
+
+    })
+  }
+  $("#toProviderDetail").on("click",function(){
+    console.log('跳转到对应页面');
+    // window.location.href="";
+  });
 
 });
