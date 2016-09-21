3938514f210ecede431895f954dacf71fb9a1a90
ADMIN\Sarah Gao <841234604@qq.com>
2016-09-21 20:31:13 +0800

    活动下载页的修改

--- src/js/activity/activity.js
+++ src/js/activity/activity.js
@@ -1,29 +1,37 @@
 $(function(){
 	// 下载页判断设备
-    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
-        $(".double_btn").html('<a href="https://itunes.apple.com/cn/app/id1066586831?mt=8"><img src="/images/activity/btn_ios.png" alt="" class="btn_down"></a>');
-    }else{
-        $(".double_btn").html('<a href="http://copen.zhujiash.com/Download/android_client.apk"><img src="/images/activity/btn_android.png" alt="" class="btn_down"></a>');
-    }   
+    if(/(micromessenger)/i.test(navigator.userAgent)){
 
-    $(".btn_down").click(function(){
+        $("#weixin-tip").show();
+        $(".btn_down").click(function(){
+            $("#weixin-tip").show();
+        });
+        $("#weixin-tip").click(function(){
+            $("#weixin-tip").hide();
+        });
+
+    }
+
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {  
+
+        $(".double_btn").html('<a href="https://itunes.apple.com/cn/app/id1066586831?mt=8"><img src="/images/activity/btn_ios.png" alt="" class="btn_down"></a>');
+        window.location.href="https://itunes.apple.com/cn/app/id1066586831?mt=8";
+    }else if{
+        $(".double_btn").html('<a href="http://copen.zhujiash.com/Download/android_client.apk"><img src="/images/activity/btn_android.png" alt="" class="btn_down"></a>');
+        window.location.href="http://copen.zhujiash.com/Download/android_client.apk";
+    }  
+
+    $(".double_btn").click(function(){
         if(/(micromessenger)/i.test(navigator.userAgent)){
             $("#weixin-tip").show();
-
             $(".btn_down").click(function(){
                 $("#weixin-tip").show();
-            })
+            });
             $("#weixin-tip").click(function(){
                 $("#weixin-tip").hide();
-            })
+            });
         }
-    })
+    });
 
-    // 列表页点击下单按钮后禁用
-    $(".item a").each(function(){
-    	$(this).click(function(){
-    		$(this).attr("href","javascript:void(0)");
-    	});
-    })
 
 })
