1442949872524d6ce0c881ccdf44c387cc1662bb
ADMIN\Sarah Gao <841234604@qq.com>
2016-09-21 15:56:17 +0800

    business-detail  business-message 页面对苹果5的适配完善。活动页面的下载页面的完善

--- src/js/activity/activity.js
+++ src/js/activity/activity.js
@@ -1,10 +1,23 @@
 $(function(){
 	// 下载页判断设备
     if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
-        $(".double_btn").html('<a href="javascript:;"><img src="/images/activity/btn_ios.png" alt="" class="btn_down"></a>');
-    } else{
-        $(".double_btn").html('<a href="javascript:;"><img src="/images/activity/btn_android.png" alt="" class="btn_down"></a>');
+        $(".double_btn").html('<a href="https://itunes.apple.com/cn/app/id1066586831?mt=8"><img src="/images/activity/btn_ios.png" alt="" class="btn_down"></a>');
+    }else{
+        $(".double_btn").html('<a href="http://copen.zhujiash.com/Download/android_client.apk"><img src="/images/activity/btn_android.png" alt="" class="btn_down"></a>');
     }   
+
+    $(".btn_down").click(function(){
+        if(/(micromessenger)/i.test(navigator.userAgent)){
+            $("#weixin-tip").show();
+
+            $(".btn_down").click(function(){
+                $("#weixin-tip").show();
+            })
+            $("#weixin-tip").click(function(){
+                $("#weixin-tip").hide();
+            })
+        }
+    })
 
     // 列表页点击下单按钮后禁用
     $(".item a").each(function(){
@@ -12,4 +25,5 @@ $(function(){
     		$(this).attr("href","javascript:void(0)");
     	});
     })
+
 })
