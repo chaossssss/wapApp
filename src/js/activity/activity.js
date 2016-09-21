5c515c1405638e5c11fb5c0fd78d36cc32842b0c
ADMIN\Sarah Gao <841234604@qq.com>
2016-09-21 21:20:29 +0800

    BUG修改好了 是因为 else if后面没写条件 去掉了if

--- src/js/activity/activity.js
+++ src/js/activity/activity.js
@@ -1,37 +1,37 @@
 $(function(){
-	// 下载页判断设备
-    // if(/(micromessenger)/i.test(navigator.userAgent)){
+	//下载页判断设备
+    if(/(micromessenger)/i.test(navigator.userAgent)){
 
-    //     $("#weixin-tip").show();
-    //     $(".btn_down").click(function(){
-    //         $("#weixin-tip").show();
-    //     });
-    //     $("#weixin-tip").click(function(){
-    //         $("#weixin-tip").hide();
-    //     });
+        $("#weixin-tip").show();
+        $(".double_btn").click(function(){
+            $("#weixin-tip").show();
+        });
+        $("#weixin-tip").click(function(){
+            $("#weixin-tip").hide();
+        });
 
-    // }
+    }
 
-    // if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {  
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)){  
 
-    //     $(".double_btn").html('<a href="https://itunes.apple.com/cn/app/id1066586831?mt=8"><img src="/images/activity/btn_ios.png" alt="" class="btn_down"></a>');
-    //     window.location.href="https://itunes.apple.com/cn/app/id1066586831?mt=8";
-    // }else if{
-    //     $(".double_btn").html('<a href="http://copen.zhujiash.com/Download/android_client.apk"><img src="/images/activity/btn_android.png" alt="" class="btn_down"></a>');
-    //     window.location.href="http://copen.zhujiash.com/Download/android_client.apk";
-    // }  
+        $(".double_btn").html('<a href="https://itunes.apple.com/cn/app/id1066586831?mt=8"><img src="/images/activity/btn_ios.png" alt="" class="btn_down"></a>');
+        window.location.href="https://itunes.apple.com/cn/app/id1066586831?mt=8";
+    }else{
+        $(".double_btn").html('<a href="http://copen.zhujiash.com/Download/android_client.apk"><img src="/images/activity/btn_android.png" alt="" class="btn_down"></a>');
+        window.location.href="http://copen.zhujiash.com/Download/android_client.apk";
+    }  
 
-    // $(".double_btn").click(function(){
-    //     if(/(micromessenger)/i.test(navigator.userAgent)){
-    //         $("#weixin-tip").show();
-    //         $(".btn_down").click(function(){
-    //             $("#weixin-tip").show();
-    //         });
-    //         $("#weixin-tip").click(function(){
-    //             $("#weixin-tip").hide();
-    //         });
-    //     }
-    // });
+    $(".double_btn").click(function(){
+        if(/(micromessenger)/i.test(navigator.userAgent)){
+            $("#weixin-tip").show();
+            $(".btn_down").click(function(){
+                $("#weixin-tip").show();
+            });
+            $("#weixin-tip").click(function(){
+                $("#weixin-tip").hide();
+            });
+        }
+    });
 
 
 })
