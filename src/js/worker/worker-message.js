4098bec5727ff4adc1d467f82f46c85921ca35ad
sunhuafeng <1503544862@qq.com>
2016-09-22 11:18:48 +0800

    工人详情

--- src/js/worker/worker-message.js
+++ src/js/worker/worker-message.js
@@ -1,20 +1,18 @@
 /**
  * Created by Administrator on 2016/9/13.
  */
+"use strict"
 $(function() {
     var Type = getUrl('type');
     var Id = getUrl('markid');
     $.ajax({
-
-
         dataType: "json",
         type: "POST",
         url: "http://192.168.1.191:3001/api/v2/Provider/Detail",
         data: {
-            Type: "Type",
-            Id: "Id"
+            Type:Type,
+            Id:Id
         },
-
         success: function (data) {
             var api = data.Body.Worker;
             $(".JobNumber").text(api.JobNumber);
@@ -33,7 +31,7 @@ $(function() {
             $(".Address").text(api.Address);
             var ModelRzImgs = "";
             for (var j = 0; j < api.SystemCertification.length; j++) {
-                ModelRzImgs += '<li><img src="' + api.SystemCertification[j].pic + '" class="sys-img" alt=""><span class="rz-name">' + api.SystemCertification[j].Name + '</span></li>';
+                ModelRzImgs += '<li><img src="' + api.SystemCertification[j].Image + '" class="sys-img" alt=""><span class="rz-name">' + api.SystemCertification[j].Name + '</span></li>';
             }
             $(".items").html(ModelRzImgs);
         },
@@ -41,29 +39,29 @@ $(function() {
         //    console("type");
         //}
     })
-
+    // 获取商户标签
     $.ajax({
         type: 'POST',
-        url: 'http://192.168.1.191:3001/api/v2/Business/GetMerchantTags',
+        url: 'http://192.168.1.191:3001/api/v2/Worker/GetWorkerTags',
         dataType: 'json',
-        data: {
-            BusinessId: Id
+        data:{
+            WorkerId:"1474"
         },
-        success: function (data) {
-            var api = data.Body.TagList;
-            var res = "";
+        success:function(data){
+            var api=data.Body.TagList;
+            var res="";
             for (var i = 0; i < api.length; i++) {
-                res += "<li>" + api[i].TagName + "</li>";
+                res+="<li>"+api[i].TagName+"</li>";
             }
 
-            if (api.length == 0) {
-                $(".business-tag").css("display", "none");
-                $(".hide-line").css("display", "none");
+            if(api.length==0){
+                $(".business-tag").css("display","none");
+                $(".hide-line").css("display","none");
             }
             $(".tag-item").html(res);
 
         },
-        error: function (xhr, type) {
+        error: function(xhr, type){
             alert('Ajax error!');
             // 即使加载出错，也得重置
             me.resetload();
@@ -71,9 +69,9 @@ $(function() {
     });
 });
 // 获取地址传参
-function getUrl(name)
-{
-    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+function getUrl(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
     var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r!=null) return unescape(r[2]); return null; //返回参数值
+    if (r != null) return unescape(r[2]);
+    return null; //返回参数值
 }
