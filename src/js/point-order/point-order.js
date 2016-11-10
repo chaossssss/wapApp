b58b8445f8de77dfd59b763e9142a9de36b4e272
zhouhao <2153229687@qq.com>
2016-11-10 17:21:06 +0800

    修改一些不知所谓的bug

--- src/js/point-order/point-order.js
+++ src/js/point-order/point-order.js
@@ -60,7 +60,7 @@ angular.module('com.wapapp.app',[])
 			if(vm.Total < 3 && (vm.serviceTypeObj.ServiceTypeId =='5' || vm.serviceTypeObj.ServiceTypeId == '707')){
 				vm.Total = 3;
 			}
-		}
+		} 
 		// if(vm.serviceTypeObj){
 		// 	console.log("服务类型id",vm.serviceTypeObj.ServiceTypeId);
 			
@@ -197,7 +197,15 @@ angular.module('com.wapapp.app',[])
 				.success(function(res){
 					console.log("获取服务说明",res);
 					if(res.Meta.ErrorCode === "0"){
-						fw = $scope.fw = res.Body;
+						// fw = $scope.fw = res.Body;
+						var descriptionList = [];
+						descriptionList.push(res.Body.Description.slice(0,res.Body.Description.indexOf("2")));
+						descriptionList.push(res.Body.Description.slice(res.Body.Description.indexOf("2"),res.Body.Description.indexOf("3")));
+						descriptionList.push(res.Body.Description.slice(res.Body.Description.indexOf("3"),res.Body.Description.indexOf("4")));
+						descriptionList.push(res.Body.Description.slice(res.Body.Description.indexOf("4"),res.Body.Description.indexOf("5")));
+						descriptionList.push(res.Body.Description.slice(res.Body.Description.indexOf("5"),res.Body.Description.indexOf("6")));
+						descriptionList.push(res.Body.Description.slice(res.Body.Description.indexOf("6"),res.Body.Description.indexOf("7")));
+						fw = $scope.fw = descriptionList;
 					}else{
 						fw = $scope.fw = null;
 					}	
