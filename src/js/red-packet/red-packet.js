a7598f576155c71a8657eb61eedc1fdfcda06dbc
13148366695 <841234604@qq.com>
2016-12-06 17:43:02 +0800

    在我的个人中心新增 我的优惠券入口

--- src/js/red-packet/red-packet.js
+++ src/js/red-packet/red-packet.js
@@ -23,48 +23,52 @@ $(function(){
         },
         success:function(data){
         	var api=data.Body.CouponList;
-        	res_0="";
-        	res_1="";
-        	res_2="";
-        	var ServiceNames=""; //品类名称
+            if(api.length>0){
+                res_0="";
+                res_1="";
+                res_2="";
+                var ServiceNames=""; //品类名称
 
-        	for (var i=0;i<api.length;i++) {
-        		if(api[i].ServiceTypes.length==0){
-        			ServiceNames="全品类使用";
-        		}else{
-        			for(var j=0;j<api[i].ServiceTypes.length;j++){
-	        			ServiceNames+=api[i].ServiceTypes[j].ServiceName;
-	        		}
-        		}
-        		
-        		var IsUsed=api[i].IsUsed; //是否可用
-        		var Id=api[i].Id;
-        		var DiscountAmount=parseInt(api[i].CouponDetails[0].DiscountAmount);// 红包金额
-        		var Amount=parseInt(api[i].CouponDetails[0].Amount); //满减金额
-        		var StartTime=api[i].StartTime.substr(0,10); //起始时间
-        		var EndTime=api[i].EndTime.substr(0,10);  //结束时间
- 		
-        		var now_time=Date.parse(new Date())/1000; //当前时间的时间戳
-        		var end_time=Date.parse(EndTime)/1000;    //结束时间时间戳
-        		var flag=(now_time<end_time)?true:false;  //true是未使用  false是已过期
+                for (var i=0;i<api.length;i++) {
+                    if(api[i].ServiceTypes.length==0){
+                        ServiceNames="全品类使用";
+                    }else{
+                        for(var j=0;j<api[i].ServiceTypes.length;j++){
+                            ServiceNames+=api[i].ServiceTypes[j].ServiceName;
+                        }
+                    }
+                    
+                    var IsUsed=api[i].IsUsed; //是否可用
+                    var Id=api[i].Id;
+                    var DiscountAmount=parseInt(api[i].CouponDetails[0].DiscountAmount);// 红包金额
+                    var Amount=parseInt(api[i].CouponDetails[0].Amount); //满减金额
+                    var StartTime=api[i].StartTime.substr(0,10); //起始时间
+                    var EndTime=api[i].EndTime.substr(0,10);  //结束时间
+            
+                    var now_time=Date.parse(new Date())/1000; //当前时间的时间戳
+                    var end_time=Date.parse(EndTime)/1000;    //结束时间时间戳
+                    var flag=(now_time<end_time)?true:false;  //true是未使用  false是已过期
 
 
-        		// 未使用
-        		if(IsUsed==0 && flag){
-					res_0+='<div class="red-item" Id='+Id+' DiscountAmount='+DiscountAmount+' Amount='+Amount+'><div class="item-left"><div class="price"><span class="rmb">￥</span><span class="DiscountAmount">'+DiscountAmount+'</span></div><div class="Amount">满'+Amount+'可用</div></div><div class="item-right"><div class="red-name">闪付红包</div><ul class="red-rule"><li class="ServiceTypes">'+ServiceNames+'</li><li><span class="StartTime">'+StartTime+'</span>至<span class="EndTime">'+EndTime+'</span></li></ul></div></div>';
-					$(".red-list").html(res_0);
-        		}
+                    // 未使用
+                    if(IsUsed==0 && flag){
+                        res_0+='<div class="red-item" Id='+Id+' DiscountAmount='+DiscountAmount+' Amount='+Amount+'><div class="item-left"><div class="price"><span class="rmb">￥</span><span class="DiscountAmount">'+DiscountAmount+'</span></div><div class="Amount">满'+Amount+'可用</div></div><div class="item-right"><div class="red-name">闪付红包</div><ul class="red-rule"><li class="ServiceTypes">'+ServiceNames+'</li><li><span class="StartTime">'+StartTime+'</span>至<span class="EndTime">'+EndTime+'</span></li></ul></div></div>';
+                        $(".red-list").html(res_0);
+                    }
 
-				// 已使用
-        		if(IsUsed==1){
-    				res_1+='<div class="red-item" Id='+Id+' DiscountAmount='+DiscountAmount+' Amount='+Amount+'><div class="item-left"><div class="price"><span class="rmb">￥</span><span class="DiscountAmount">'+DiscountAmount+'</span></div><div class="Amount">满'+Amount+'可用</div></div><div class="item-right"><div class="red-name">闪付红包</div><ul class="red-rule"><li class="ServiceTypes">'+ServiceNames+'</li><li><span class="StartTime">'+StartTime+'</span>至<span class="EndTime">'+EndTime+'</span></li></ul></div></div>';
-        		}
-        		// 已过期
-        		if(now_time>end_time){
-        			res_2+='<div class="red-item" Id='+Id+' DiscountAmount='+DiscountAmount+' Amount='+Amount+'><div class="item-left"><div class="price"><span class="rmb">￥</span><span class="DiscountAmount">'+DiscountAmount+'</span></div><div class="Amount">满'+Amount+'可用</div></div><div class="item-right"><div class="red-name">闪付红包</div><ul class="red-rule"><li class="ServiceTypes">'+ServiceNames+'</li><li><span class="StartTime">'+StartTime+'</span>至<span class="EndTime">'+EndTime+'</span></li></ul></div></div>';
-        		}
-		
-        	};    	
+                    // 已使用
+                    if(IsUsed==1){
+                        res_1+='<div class="red-item" Id='+Id+' DiscountAmount='+DiscountAmount+' Amount='+Amount+'><div class="item-left"><div class="price"><span class="rmb">￥</span><span class="DiscountAmount">'+DiscountAmount+'</span></div><div class="Amount">满'+Amount+'可用</div></div><div class="item-right"><div class="red-name">闪付红包</div><ul class="red-rule"><li class="ServiceTypes">'+ServiceNames+'</li><li><span class="StartTime">'+StartTime+'</span>至<span class="EndTime">'+EndTime+'</span></li></ul></div></div>';
+                    }
+                    // 已过期
+                    if(now_time>end_time){
+                        res_2+='<div class="red-item" Id='+Id+' DiscountAmount='+DiscountAmount+' Amount='+Amount+'><div class="item-left"><div class="price"><span class="rmb">￥</span><span class="DiscountAmount">'+DiscountAmount+'</span></div><div class="Amount">满'+Amount+'可用</div></div><div class="item-right"><div class="red-name">闪付红包</div><ul class="red-rule"><li class="ServiceTypes">'+ServiceNames+'</li><li><span class="StartTime">'+StartTime+'</span>至<span class="EndTime">'+EndTime+'</span></li></ul></div></div>';
+                    }       
+                }; 
+            }else{
+                $(".no-hb").css("display","block");
+            }
+        	   	
             
         },
         error: function(xhr, type){
@@ -72,16 +76,23 @@ $(function(){
 	});
 
 	$("#unuse").click(function(){
-		$(".red-list").html(res_0);
+        if(typeof(res_0)!="undefined"){
+            $(".red-list").html(res_0);
+        }
+		
 	})
 	$("#used").click(function(){
-		$(".red-list").html(res_1);
-		NoUse();
+        if(typeof(res_1)!="undefined"){
+    		$(".red-list").html(res_1);
+    		NoUse();
+        }
 
 	})
 	$("#delay").click(function(){
-		$(".red-list").html(res_2);
-		NoUse();
+        if(typeof(res_2)!="undefined"){
+    		$(".red-list").html(res_2);
+    		NoUse();
+        }
 	})
 
 })
